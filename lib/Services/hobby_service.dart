0d44272baec46ec243ac032bf2701dd0901acd79
Zaid Alsaid <zaidalsaid11@gmail.com>
2021-02-15 15:08:05 +0200

    add fetchHobby method

--- lib/Services/hobby_service.dart
+++ lib/Services/hobby_service.dart
@@ -18,4 +18,16 @@ class HobbyService extends ChangeNotifier {
       throw Exception("Unable to get hobbies!");
     }
   }
+
+  Future<Hobby> fetchHobby(String hobbyName) async {
+    final url = "http://10.0.2.2:8080/api/hobbies/$hobbyName";
+    final response = await get(url);
+    if (response.statusCode == 200) {
+      List<Hobby> hobbiesList = [];
+      final body = jsonDecode(response.body);
+      return Hobby.fromJson(body);
+    } else {
+      throw Exception("Unable to get hobbies!");
+    }
+  }
 }
