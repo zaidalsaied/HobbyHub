4c798a943244cfce4cfae54edadbd4efcd96c25d
Zaid Alsaid <zaidalsaid11@gmail.com>
2021-02-15 19:11:28 +0200

    add getters for hobby model

--- lib/Services/hobby_service.dart
+++ lib/Services/hobby_service.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/cupertino.dart';
 import 'package:hobby_hub_ui/models/hobby.dart';
 import 'package:http/http.dart';
@@ -23,7 +22,6 @@ class HobbyService extends ChangeNotifier {
     final url = "http://10.0.2.2:8080/api/hobbies/$hobbyName";
     final response = await get(url);
     if (response.statusCode == 200) {
-      List<Hobby> hobbiesList = [];
       final body = jsonDecode(response.body);
       return Hobby.fromJson(body);
     } else {
--- lib/models/hobby.dart
+++ lib/models/hobby.dart
@@ -1,12 +1,14 @@
 class Hobby {
-  String hobbyId;
-  String hobbyName;
-  String imageUrl;
-  List<String> followers;
-  String description;
+  String _hobbyId;
+  String _hobbyName;
+  String _imageUrl;
+  List<String> _followers;
+  String _description;
 
-  Hobby(this.hobbyId, this.hobbyName, this.imageUrl, this.description,
-      this.followers);
+  Hobby(this._hobbyId, this._hobbyName, this._imageUrl, this._description,
+      this._followers);
+
+  String get hobbyId => _hobbyId;
 
   factory Hobby.fromJson(Map<String, dynamic> json) {
     return Hobby(
@@ -17,4 +19,16 @@ class Hobby {
       List<String>.from(json["followers"]),
     );
   }
+
+  String get hobbyName => _hobbyName;
+
+  String get description => _description;
+
+  List<String> get followers {
+    List<String> followersList = [];
+    for (String user in _followers) followersList.add(user);
+    return followersList;
+  }
+
+  String get imageUrl => _imageUrl;
 }
